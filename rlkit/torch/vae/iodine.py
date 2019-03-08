a60f1bfd5a642c377522601eb9e4de90bb1e5048
John Co-Reyes <jdcoreyes@gmail.com>
2019-03-07 16:10:10 -0800

    Iodine example running. Need to test convergence and add lambda grads

--- rlkit/torch/vae/iodine.py
+++ rlkit/torch/vae/iodine.py
@@ -10,28 +10,15 @@ from rlkit.torch.core import PyTorchModule
 from rlkit.torch.pytorch_util import from_numpy
 from rlkit.torch.conv_networks import CNN, DCNN
 from rlkit.torch.vae.vae_base import GaussianLatentVAE
+from rlkit.torch.modules import LayerNorm2D
 
-imsize84_monet_architecture = dict(
-    conv_args=dict(
-        kernel_sizes=[3, 3, 3, 3],
-        n_channels=[32, 32, 64, 64],
-        strides=[2, 2, 2, 2],
-    ),
-    conv_kwargs=dict(
-        hidden_sizes=[256, 32],
-        batch_norm_conv=False,
-        batch_norm_fc=False,
-    ),
+imsize84_iodine_architecture = dict(
     deconv_args=dict(
         hidden_sizes=[],
 
         input_width=92,
         input_height=92,
-        input_channels=18,
-
-        # deconv_output_kernel_size=6,
-        # deconv_output_strides=3,
-        # deconv_output_channels=3,
+        input_channels=130,
 
         kernel_sizes=[3, 3, 3, 3, 1],
         n_channels=[32, 32, 32, 32, 4],
@@ -40,10 +27,23 @@ imsize84_monet_architecture = dict(
     deconv_kwargs=dict(
         batch_norm_conv=False,
         batch_norm_fc=False,
+    ),
+    refine_args=dict(
+        input_width=84,
+        input_height=84,
+        input_channels=19,
+        paddings=[0, 0, 0, 0],
+        kernel_sizes=[3, 3, 3, 3],
+        n_channels=[32, 32, 64, 64],
+        strides=[2, 2, 2, 2],
+        hidden_sizes=[256, 256],
+        output_size=256,
+        lstm_size=256,
+
     )
 )
 
-imsize64_monet_architecture = dict(
+imsize64_iodine_architecture = dict(
     conv_args=dict(
         kernel_sizes=[3, 3, 3, 3],
         n_channels=[32, 32, 64, 64],
@@ -77,13 +77,12 @@ imsize64_monet_architecture = dict(
 
 
 
-class MonetVAE(GaussianLatentVAE):
+class IodineVAE(GaussianLatentVAE):
     def __init__(
             self,
             representation_size,
             architecture,
-            attention_net,
-            encoder_class=CNN,
+            refinement_net,
             decoder_class=DCNN,
             decoder_output_activation=identity,
             decoder_distribution='bernoulli',
@@ -93,6 +92,7 @@ class MonetVAE(GaussianLatentVAE):
             init_w=1e-3,
             min_variance=1e-3,
             hidden_init=ptu.fanin_init,
+            beta=0.5,
     ):
         """
 
@@ -139,34 +139,10 @@ class MonetVAE(GaussianLatentVAE):
         self.input_channels = input_channels
         self.imsize = imsize
         self.imlength = self.imsize * self.imsize * self.input_channels
-        self.attention_net = attention_net
+        self.refinement_net = refinement_net
+        self.beta = beta
 
-        conv_args, conv_kwargs, deconv_args, deconv_kwargs = \
-            architecture['conv_args'], architecture['conv_kwargs'], \
-            architecture['deconv_args'], architecture['deconv_kwargs']
-        # conv_output_size = deconv_args['deconv_input_width'] * \
-        #                    deconv_args['deconv_input_height'] * \
-        #                    deconv_args['deconv_input_channels']
-
-        self.encoder = encoder_class(
-            **conv_args,
-            paddings=np.zeros(len(conv_args['kernel_sizes']), dtype=np.int64),
-            input_height=self.imsize,
-            input_width=self.imsize,
-            input_channels=self.input_channels,
-            output_size=self.imlength,
-            init_w=init_w,
-            hidden_init=hidden_init,
-            **conv_kwargs)
-
-        self.fc1 = nn.Linear(self.encoder.output_size, representation_size)
-        self.fc2 = nn.Linear(self.encoder.output_size, representation_size)
-
-        self.fc1.weight.data.uniform_(-init_w, init_w)
-        self.fc1.bias.data.uniform_(-init_w, init_w)
-
-        self.fc2.weight.data.uniform_(-init_w, init_w)
-        self.fc2.bias.data.uniform_(-init_w, init_w)
+        deconv_args, deconv_kwargs = architecture['deconv_args'], architecture['deconv_kwargs']
 
         self.decoder = decoder_class(
             **deconv_args,
@@ -174,19 +150,17 @@ class MonetVAE(GaussianLatentVAE):
             output_size=self.imlength,
             init_w=init_w,
             hidden_init=hidden_init,
+            hidden_activation=nn.ELU(),
             **deconv_kwargs)
 
+        l_norm_sizes = [3, 1, 1, 1, 1]
+        self.layer_norms = [LayerNorm2D(l) for l in l_norm_sizes]
+        [l.to(ptu.device) for l in self.layer_norms]
         self.epoch = 0
         self.decoder_distribution = decoder_distribution
 
     def encode(self, input):
-        h = self.encoder(input)
-        mu = self.fc1(h)
-        if self.log_min_variance is None:
-            logvar = self.fc2(h)
-        else:
-            logvar = self.log_min_variance + torch.abs(self.fc2(h))
-        return (mu, logvar)
+        pass
 
     def decode(self, latents):
         decoded = self.decoder(latents)
@@ -211,70 +185,69 @@ class MonetVAE(GaussianLatentVAE):
     def logprob(self, inputs, obs_distribution_params):
         pass
 
-    def forward(self, input):
+    def forward(self, input, optimizer):
         """
         :param input:
         :return: reconstructed input, obs_distribution_params, latent_distribution_params
         """
         K = 3
+        T = 5
         bs = input.shape[0]
-        scope = from_numpy(np.ones((bs, 1, self.imsize, self.imsize)))
-        log_scope = torch.log(scope)
+        
+        
         input = input.view(bs, 3, self.imsize, self.imsize)
-        x_prob_losses = []
-        kle_losses = []
-        x_hats = []
-        masks = []
-        m_hat_logits = []
-        log_masks = []
-        for k in range(K):
+        inputK = torch.cat([input for _ in range(K)], 0) # detach or create copy here?
+        sigma = 0.25
+        # means and log_vars of latent
+        lambdas = [from_numpy(np.zeros((bs, K, self.representation_size))),
+                   from_numpy(np.zeros((bs, K, self.representation_size)))]
+        # initialize hidden state
+        h = self.refinement_net.initialize_hidden(bs*K)
+        optimizer.zero_grad()
+        lns = self.layer_norms
+        for t in range(T):
+            z = self.reparameterize(lambdas)
+            x_hat, x_var_hat, m_hat_logit = self.decode(z.view(bs*K, self.representation_size))
+            m_hat_logit = m_hat_logit.view(bs, K, self.imsize, self.imsize)
+            mask = F.softmax(m_hat_logit, dim=1)
+            # Retain grads
+            mask.retain_grad()
+            x_hat.retain_grad()
+            [l.retain_grad() for l in lambdas]
 
-            if k < K - 1:
-                attention_input = torch.cat([input, log_scope], 1)
-                attention_logits = self.attention_net(attention_input)
-                log_mask = log_scope + F.logsigmoid(attention_logits)
-            else:
-                log_mask = log_scope
+            x_prob = self.gaussian_prob(x_hat, inputK, from_numpy(np.array([sigma])))
+            likelihood = (mask.unsqueeze(2) * x_prob.view(bs, K, 3, self.imsize, self.imsize))
+            log_likelihood = -torch.log(likelihood.sum(1)).sum() / bs
+            kle = self.kl_divergence([l.view(bs*K, self.representation_size) for l in lambdas])
+            kle_loss = self.beta * kle.sum() / bs
+            loss = kle_loss + log_likelihood
+            # Compute gradients
+            loss.backward(retain_graph=True)
 
-            if k == 0:
-                sigma = 0.09
-            else:
-                sigma = 0.11
-            mask = torch.exp(log_mask)
+            # Compute inputs a for refinement network
+            pixel_likelihood = torch.prod(likelihood, 2) # product among color channels
+            posterior_mask = pixel_likelihood / pixel_likelihood.sum(1, keepdim=True)
+            leave_out_ll = pixel_likelihood.sum(1, keepdim=True) - pixel_likelihood
 
-            vae_input = torch.cat([input, log_mask], 1)
-            latent_distribution_params = self.encode(vae_input.view(bs, -1))
-            latents = self.reparameterize(latent_distribution_params)
-            x_hat, x_var_hat, m_hat_logit = self.decode(latents)
+            tiled_k_shape = (bs * K, -1, self.imsize, self.imsize)
+            a = torch.cat([
+                inputK, x_hat, mask.view(tiled_k_shape), m_hat_logit.view(tiled_k_shape),
+                lns[0](x_hat.grad.detach()),
+                lns[1](mask.grad.view(tiled_k_shape).detach()),
+                lns[2](posterior_mask.view(tiled_k_shape)),
+                lns[3](pixel_likelihood.unsqueeze(1).repeat(1, K, 1, 1, 1).view(tiled_k_shape).detach()),
+                lns[4](leave_out_ll.view(tiled_k_shape).detach())
+            ], 1)
 
-            x_hats.append(x_hat.detach())
-            masks.append(mask.detach())
-            m_hat_logits.append(torch.unsqueeze(m_hat_logit, 1))
-            log_masks.append(log_mask)
+            refinement, h = self.refinement_net(a, h)
+            refinement = refinement.view(bs, K, self.representation_size*2) # updates for both means and log_vars
+            lambdas[0] = lambdas[0] + refinement[:, :, :self.representation_size]
+            lambdas[1] = lambdas[1] + refinement[:, :, self.representation_size:]
 
-            # 1. m outside log
-            #x_prob = self.gaussian_log_prob(x_hat, input, from_numpy(np.array([sigma])))
-            #x_prob_losses.append(mask * x_prob)
+        torch.nn.utils.clip_grad_norm_(self.parameters(), 5) # TODO Clip other gradients?
+        optimizer.step()
 
-            # 2. m inside log
-            x_prob = self.gaussian_prob(x_hat, input, from_numpy(np.array([sigma])))
-            x_prob_losses.append(mask * x_prob)
+        return x_hat, mask, loss, kle_loss, log_likelihood
 
-            kle = self.kl_divergence(latent_distribution_params)
-            kle_losses.append(kle)
-
-            log_scope = log_scope + F.logsigmoid(-attention_logits) # log(s(1-attention)) = log(s) + log (1-attention)
-
-        #import pdb; pdb.set_trace()
-        all_m_hat_logits = torch.cat(m_hat_logits, 1)
-        all_log_masks = torch.cat(log_masks, 1)
-        mask_loss = F.softmax(all_m_hat_logits, 1) * (F.log_softmax(all_m_hat_logits, 1) - all_log_masks)
-        mask_loss = mask_loss.sum() / input.shape[0]
-
-        reconstruction = x_hats[0] * masks[0]
-        for k in range(1, K):
-            reconstruction += x_hats[k] * masks[k]
-
-        return reconstruction, x_prob_losses, kle_losses, mask_loss, x_hats, masks
 
 
---
+++ rlkit/torch/vae/refinement_network.py
@@ -0,0 +1,150 @@
+import torch
+import torch.utils.data
+from rlkit.torch.pytorch_util import from_numpy
+from torch import nn
+from torch.nn import functional as F
+from rlkit.pythonplusplus import identity
+from rlkit.torch import pytorch_util as ptu
+import numpy as np
+from rlkit.torch.core import PyTorchModule
+
+class RefinementNetwork(PyTorchModule):
+    def __init__(
+            self,
+            input_width,
+            input_height,
+            input_channels,
+            output_size,
+            kernel_sizes,
+            n_channels,
+            strides,
+            paddings,
+            hidden_sizes,
+            lstm_size,
+            added_fc_input_size=0,
+            batch_norm_conv=False,
+            batch_norm_fc=False,
+            init_w=1e-4,
+            hidden_init=nn.init.xavier_uniform_,
+            hidden_activation=nn.ReLU(),
+            output_activation=identity,
+    ):
+        if hidden_sizes is None:
+            hidden_sizes = []
+        assert len(kernel_sizes) == \
+               len(n_channels) == \
+               len(strides) == \
+               len(paddings)
+        self.save_init_params(locals())
+        super().__init__()
+
+        self.hidden_sizes = hidden_sizes
+        self.input_width = input_width
+        self.input_height = input_height
+        self.input_channels = input_channels
+        self.lstm_size = lstm_size
+        self.output_size = output_size
+        self.output_activation = output_activation
+        self.hidden_activation = hidden_activation
+        self.batch_norm_conv = batch_norm_conv
+        self.batch_norm_fc = batch_norm_fc
+        self.added_fc_input_size = added_fc_input_size
+        self.conv_input_length = self.input_width * self.input_height * self.input_channels
+
+        self.conv_layers = nn.ModuleList()
+        self.conv_norm_layers = nn.ModuleList()
+        self.fc_layers = nn.ModuleList()
+        self.fc_norm_layers = nn.ModuleList()
+
+        self.lstm = nn.LSTM(hidden_sizes[-1], lstm_size, num_layers=1, batch_first=True)
+
+        for out_channels, kernel_size, stride, padding in \
+                zip(n_channels, kernel_sizes, strides, paddings):
+            conv = nn.Conv2d(input_channels,
+                             out_channels,
+                             kernel_size,
+                             stride=stride,
+                             padding=padding)
+            hidden_init(conv.weight)
+            conv.bias.data.fill_(0)
+
+            conv_layer = conv
+            self.conv_layers.append(conv_layer)
+            input_channels = out_channels
+
+        # find output dim of conv_layers by trial and add normalization conv layers
+        test_mat = torch.zeros(1, self.input_channels, self.input_width,
+                               self.input_height)  # initially the model is on CPU (caller should then move it to GPU if
+        for conv_layer in self.conv_layers:
+            test_mat = conv_layer(test_mat)
+            self.conv_norm_layers.append(nn.BatchNorm2d(test_mat.shape[1]))
+
+        fc_input_size = int(np.prod(test_mat.shape))
+        # used only for injecting input directly into fc layers
+        fc_input_size += added_fc_input_size
+
+        for idx, hidden_size in enumerate(hidden_sizes):
+            fc_layer = nn.Linear(fc_input_size, hidden_size)
+
+            norm_layer = nn.BatchNorm1d(hidden_size)
+            fc_layer.weight.data.uniform_(-init_w, init_w)
+            fc_layer.bias.data.uniform_(-init_w, init_w)
+
+            self.fc_layers.append(fc_layer)
+            self.fc_norm_layers.append(norm_layer)
+            fc_input_size = hidden_size
+
+        self.last_fc = nn.Linear(lstm_size, output_size)
+        #self.last_fc.weight.data.uniform_(-init_w, init_w)
+        #self.last_fc.bias.data.uniform_(-init_w, init_w)
+
+        xcoords = np.expand_dims(np.linspace(-1, 1, self.input_width), 0).repeat(self.input_height, 0)
+        ycoords = np.repeat(np.linspace(-1, 1, self.input_height), self.input_width).reshape((self.input_height, self.input_width))
+
+        self.coords = np.stack([xcoords, ycoords], 0)
+
+    def forward(self, input, hidden):
+        fc_input = (self.added_fc_input_size != 0)
+
+        #conv_input = input.narrow(start=0,
+        #                          length=self.conv_input_length,
+        #                          dim=1).contiguous()
+        if fc_input:
+            extra_fc_input = input.narrow(start=self.conv_input_length,
+                                          length=self.added_fc_input_size,
+                                          dim=1)
+        # need to reshape from batch of flattened images into (channsls, w, h)
+        h = input.view(input.shape[0],
+                        self.input_channels-2,
+                        self.input_height,
+                        self.input_width)
+
+        coords = from_numpy(np.repeat(np.expand_dims(self.coords, 0), input.shape[0], 0))
+        h = torch.cat([h, coords], 1)
+
+        h = self.apply_forward(h, self.conv_layers, self.conv_norm_layers,
+                               use_batch_norm=self.batch_norm_conv)
+        # flatten channels for fc layers
+        h = h.view(h.size(0), -1)
+        if fc_input:
+            h = torch.cat((h, extra_fc_input), dim=1)
+        output = self.apply_forward(h, self.fc_layers, self.fc_norm_layers,
+                               use_batch_norm=self.batch_norm_fc)
+
+        output, hidden = self.lstm(output.unsqueeze(1), hidden)
+        output = self.output_activation(self.last_fc(output.squeeze()))
+        return output, hidden
+
+    def initialize_hidden(self, bs):
+        return (ptu.from_numpy(np.zeros((1, bs, self.lstm_size))), ptu.from_numpy(np.zeros((1, bs, self.lstm_size))))
+
+    def apply_forward(self, input, hidden_layers, norm_layers,
+                      use_batch_norm=False):
+        h = input
+        for layer, norm_layer in zip(hidden_layers, norm_layers):
+            h = layer(h)
+            if use_batch_norm:
+                h = norm_layer(h)
+            h = self.hidden_activation(h)
+        return h
+
