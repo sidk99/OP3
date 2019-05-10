Fix loss weighting

rlkit/torch/iodine/iodine.py
@@ -96,7 +96,7 @@ imsize64_large_iodine_architecture = dict(
         # decoder_distribution='gaussian_identity_variance',
         beta=1,
         K=7,
-        T=15,
+        sigma=0.08
     ),
     deconv_args=dict(
         hidden_sizes=[],
@@ -164,7 +164,6 @@ class IodineVAE(GaussianLatentVAE):
             action_dim=None,
             physics_net=None,
             K=3,
-            T=5,
             input_channels=1,
             imsize=48,
             min_variance=1e-3,
@@ -186,7 +185,6 @@ class IodineVAE(GaussianLatentVAE):
         else:
             self.log_min_variance = float(np.log(min_variance))
         self.K = K
-        self.T = T
         self.input_channels = input_channels
         self.imsize = imsize
         self.imlength = self.imsize * self.imsize * self.input_channels
@@ -392,7 +390,7 @@ class IodineVAE(GaussianLatentVAE):
                 kle = self.kl_divergence_softplus([lambdas1, lambdas2])
                 kle_loss = self.beta * kle.sum() / bs
                 loss = log_likelihood + kle_loss
-                losses.append(loss/t)
+                losses.append(loss*t)
 
                 # Compute inputs a for refinement network
                 posterior_mask = pixel_x_prob / (pixel_x_prob.sum(1, keepdim=True) + 1e-8)  # avoid divide by zero
@@ -421,7 +419,7 @@ class IodineVAE(GaussianLatentVAE):
 
                 # If haven't applied action yet and still seeding then also do physics on seed images
                 if apply_action:
-                   lambdas1, _ = self.physics_net(lambdas1, lambdas2)
+                  lambdas1, _ = self.physics_net(lambdas1, lambdas2)
 
             else:
                 z = self.rsample_softplus([lambdas1, lambdas2])
@@ -440,7 +438,7 @@ class IodineVAE(GaussianLatentVAE):
                 kle = self.kl_divergence_softplus([lambdas1, lambdas2])
                 kle_loss = self.beta * kle.sum() / bs
                 loss = log_likelihood + kle_loss
-                losses.append(loss/t)
+                losses.append(loss*t)
 
                 current_step += 1
                 current_step = min(input.shape[1]-1, current_step)
@@ -450,7 +448,7 @@ class IodineVAE(GaussianLatentVAE):
                     apply_action = False
                 lambdas1, _ = self.physics_net(lambdas1, lambdas2)
 
-        total_loss = sum(losses*T)
+        total_loss = sum(losses)/T
 
         final_recon = (mask.unsqueeze(2) * x_hat.view(untiled_k_shape)).sum(1)
         mse = torch.pow(final_recon - input[:, current_step], 2).mean()

rlkit/torch/iodine/iodine_trainer.py
@@ -18,6 +18,7 @@ class IodineTrainer(Serializable):
             model,
             seed_steps=4,
             train_T=15,
+            test_T=9,
             batch_size=128,
             log_interval=0,
             gamma=0.5,
@@ -30,6 +31,7 @@ class IodineTrainer(Serializable):
 
         self.seed_steps = seed_steps
         self.train_T = train_T
+        self.test_T = test_T
 
         model.to(ptu.device)
 
@@ -70,6 +72,7 @@ class IodineTrainer(Serializable):
             # when only doing refinement predict same image
             # when only doing physics predict next image
             schedule = np.random.randint(0, 2, (self.train_T,))
+            #schedule = np.ones((self.train_T,))
             schedule[:self.seed_steps] = 0
             #inputs = self.prepare_inputs()
             x_hat, mask, loss, kle_loss, x_prob_loss, mse, final_recon, lambdas = self.model(obs, actions=actions, schedule=schedule)
@@ -101,8 +104,8 @@ class IodineTrainer(Serializable):
             train=True,
             batches=1,
     ):
-        T = 9
-        schedule = np.ones((T,))
+
+        schedule = np.ones((self.test_T,))
         schedule[:self.seed_steps] = 0
 
         self.model.eval()
@@ -135,7 +138,7 @@ class IodineTrainer(Serializable):
 
                 save_dir = osp.join(logger.get_snapshot_dir(),
                                     '%s_r%d.png' % ('train' if train else 'val', epoch))
-                save_image(comparison.data.cpu(), save_dir, nrow=T)
+                save_image(comparison.data.cpu(), save_dir, nrow=self.test_T)
             if batch_idx >= batches - 1:
                 break
 